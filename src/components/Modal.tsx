6a4ca6a579e4534383d2a7cc2ec7f24755fb402a
Abdulmojeed <mojeedayoola58@gmail.com>
2023-09-17 17:34:34 +0100

    feat: add some extra feature

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,15 +3,18 @@ import { ReactNode } from "react";
 interface ModalProps {
   children: ReactNode;
   isOpen?: boolean;
+  handleChange?: () => void;
 }
 
-const Backdrop: React.FC<ModalProps> = ({ children, isOpen }) => {
+const Backdrop: React.FC<ModalProps> = ({ children, handleChange }) => {
   return (
-    <div className={` ${
-        isOpen ? "opacity-60" : "opacity-0"
-      } pointer-events-none fixed inset-5 z-10 overflow-hidden flex items-center justify-center`}>
+    <div
+      className={`fixed inset-5 z-10 overflow-hidden flex items-center justify-center`}
+      onClick={handleChange}
+    >
       <div
-        className={`modal bg-[#000000] w-[80vh] h-[80vh] rounded-[2rem] flex items-center justify-center transition duration-[2s] ease-in-out`}
+        style={{ backdropFilter: "blur(10px)" }}
+        className={`modal bg-[#000000b9] w-[80vh] h-[80vh] rounded-[2rem] flex items-center justify-center transition duration-[2s] ease-in-out`}
       >
         {children}
       </div>
@@ -19,9 +22,9 @@ const Backdrop: React.FC<ModalProps> = ({ children, isOpen }) => {
   );
 };
 
-const Modal: React.FC<ModalProps> = ({ children, isOpen }) => {
+const Modal: React.FC<ModalProps> = ({ children, handleChange }) => {
   return (
-    <Backdrop isOpen={isOpen}>
+    <Backdrop handleChange={handleChange}>
       <div className="bg-white">{children}</div>
     </Backdrop>
   );
