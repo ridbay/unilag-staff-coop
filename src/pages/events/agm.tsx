e073ae927c282af1e5046a9d539a292b483664b9
chibuike-19 <emmanuelobiechina8@gmail.com>
2023-10-09 13:50:03 +0100

    changed date for event

--- src/pages/events/agm.tsx
+++ src/pages/events/agm.tsx
@@ -5,15 +5,15 @@ import EventsLayout from "@/components/layouts/EventsLayout";
 const rows = [
   {
     id: "Date:",
-    title: "Thursday, 10th November, 2022",
+    title: "Thursday, 19th OCTOBER, 2023",
   },
   {
     id: "Venue:",
-    title: "	Tolu Odegbemi Hall, Staff School University of Lagos",
+    title: "J.F ADE AJAYI MAIN AUDITURIUM, University of Lagos",
   },
   {
     id: "Start:",
-    title: "9:30 AM",
+    title: "11:00 AM",
   },
 ];
 
@@ -22,16 +22,16 @@ const Agm = () => {
     <EventsLayout>
       <section>
         <h1 className="text-[2rem] font-semibold pl-4 mb-[3rem] text-center md:text-left">
-          24th Annual General Meeting (AGM)
+          25th Annual General Meeting (AGM)
         </h1>
         <div className="border border-[#0000005d]">
           <h2 className="text-center py-5 px-2 bg-[#f5f5f5]">
-            The details of the 24th Annual General Meeting of Unique Unilag
+            The details of the 25th Annual General Meeting of Unique Unilag
             Staff Cooperative Multipurpose Society Limited are as follows:
           </h2>
           <div className="border border-[#00000009] rounded-[5px] p-5 max-w-max mx-3 my-[2rem]">
             <h2 className="text-center py-5 bg-theme-color">
-              24TH ANNUAL GENERAL MEETING
+              25TH ANNUAL GENERAL MEETING
             </h2>
             <CustomizedTables data={rows} width={{large: "100%", small: "100%"}} />
           </div>
