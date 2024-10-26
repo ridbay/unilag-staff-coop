d1f092eb748486062bd91ae1868350772a4c15a9
chibuike-19 <emmanuelobiechina8@gmail.com>
2024-10-26 13:49:30 +0100

    chore: updated carousel image

--- src/pages/events/agm.tsx
+++ src/pages/events/agm.tsx
@@ -5,7 +5,7 @@ import EventsLayout from "@/components/layouts/EventsLayout";
 const rows = [
   {
     id: "Date:",
-    title: "Thursday, 19th OCTOBER, 2023",
+    title: "7th NOVEMBER, 2024",
   },
   {
     id: "Venue:",
@@ -13,7 +13,7 @@ const rows = [
   },
   {
     id: "Start:",
-    title: "11:00 AM",
+    title: "9:30 AM",
   },
 ];
 
@@ -22,16 +22,16 @@ const Agm = () => {
     <EventsLayout>
       <section>
         <h1 className="text-[2rem] font-semibold pl-4 mb-[3rem] text-center md:text-left">
-          25th Annual General Meeting (AGM)
+          26th Annual General Meeting (AGM)
         </h1>
         <div className="border border-[#0000005d]">
           <h2 className="text-center py-5 px-2 bg-[#f5f5f5]">
-            The details of the 25th Annual General Meeting of Unique Unilag
+            The details of the 26th Annual General Meeting of Unique Unilag
             Staff Cooperative Multipurpose Society Limited are as follows:
           </h2>
           <div className="border border-[#00000009] rounded-[5px] p-5 max-w-max mx-3 my-[2rem]">
             <h2 className="text-center py-5 bg-theme-color">
-              25TH ANNUAL GENERAL MEETING
+              26TH ANNUAL GENERAL MEETING
             </h2>
             <CustomizedTables data={rows} width={{large: "100%", small: "100%"}} />
           </div>
