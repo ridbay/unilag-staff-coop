523fcaa51f70469d2789fd88f10c2a9cb9066beb
Ridwan Balogun <balogunridwan@gmail.com>
2025-10-16 21:03:22 +0100

    deployment modification

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   apps: [
     {
-      script: "npm start",
+      name: "staff-coop",
+      cwd: "/home/ubuntu/source",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
     },
   ],
 
