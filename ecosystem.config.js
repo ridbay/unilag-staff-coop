e111bf3e3a9640882d11b1a674d5a8ba5c5d4638
Ridwan Balogun <balogunridwan@gmail.com>
2025-10-23 09:49:58 +0100

    cloudflare deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   apps: [
     {
-      script: "npm start",
       name: "staff-coop",
+      cwd: "/home/ubuntu/source",
+      script: "npm",
+      args: "run start -- -p 3000",
+      interpreter: "/home/ubuntu/.nvm/versions/node/v18.20.4/bin/node",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
     },
   ],
 
@@ -16,7 +23,7 @@ module.exports = {
       path: "/home/ubuntu",
       "pre-deploy-local": "",
       "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+        "source ~/.nvm/nvm.sh && nvm use 18.20.4 && cd /home/ubuntu/source && (npm ci || npm install) && npm run build && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
       ssh_options: "ForwardAgent=yes",
     },
