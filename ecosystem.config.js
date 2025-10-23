8a4dabd124858331724a1a71aa9fc529c64d032b
Ridwan Balogun <balogunridwan@gmail.com>
2025-10-23 06:53:53 +0100

    Implementing endpoint integrattion to cyrpto log

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "staff-coop",
-      cwd: "/home/ubuntu/source",
-      script: "npm",
-      args: "run start -- -p 3000",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3000,
-      },
+      script: "staff-coop",
     },
   ],
 
@@ -18,11 +11,11 @@ module.exports = {
       user: "root",
       host: "196.45.48.166",
       ref: "origin/master",
-      repo: "https://github.com/chibuike-19/unilag-staff-coop.git",
+      repo: "https://github.com/ridbay/unilag-staff-coop.git",
       path: "/home/ubuntu",
       "pre-deploy-local": "",
       "post-deploy":
-        "source ~/.nvm/nvm.sh && cd /home/ubuntu/source && (npm ci || npm install) && npm run build && pm2 reload ecosystem.config.js --env production",
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
       ssh_options: "ForwardAgent=yes",
     },
